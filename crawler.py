83b8b2ccf940fbbb13e4a116eff8813a0665ceaa
HermanKosakian <163388133+HermanKosakian@users.noreply.github.com>
2024-11-02 06:43:07 +0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from icrawler.builtin import GoogleImageCrawler
 
 def d_image(dir_name: str,keyword: str) -> None:
     """
-    Функция выполняет установку изображения с помощью GoogleImageCrawler, перед этим создает директорий(если необходимо) и очищает директорий(если необходимо)
+    Функция выполняет установку изображения с помощью GoogleImageCrawler, перед этим создает директорий(если необходимо)
     :param dir_name: директория, в которую мы сохраняем изображения
     :param keyword: ключевое слово
     :return: None
